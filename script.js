c505f2809068c70c20ce42b622f61eaec0f53213
LizardRock <lizardi.designs@gmail.com>
2015-05-23 19:46:51 -0400

    testing new site layout

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " LizardRock. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='BotM.html'>BotM</a></li>" +
+"<li><a href='Contact.html'>Contact</a></li>" +
+
+"</ul>";
