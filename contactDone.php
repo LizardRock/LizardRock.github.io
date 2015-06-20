38b2a866a6d5e8cface315eb74577569e36979d7
LizardRock <lizardi.designs@gmail.com>
2015-06-19 21:05:07 -0400

    new try

---
+++ contactDone.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<style>
+label{
+width: 4em;
+float: left;
+text-align: right;
+margin-right: 0.5em;
+display: block
+}
+</style>
+<head>
+  <title>LizardRock Contact</title>
+  <link href="site.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <nav id="nav01"></nav>
+
+  <div id="main">
+  <h1><img src="Logo.png" alt="LizardRock Symbol" 
+style="width:50px;height:50px">Contact Got</h1>
+  <p>Your Contact has been recieved and processed, I try to check my forms daily, so expect a response (if requested) within 24 hours.
+  </p>
+
+ 
+  </div>
+<footer id="foot01"></footer>
+  						
+
+<script src="script.js"></script>
+
+</body>
+</html>
--- contactForm.php
+++ contactForm.php
@@ -1,42 +1,17 @@
-<!DOCTYPE html>
-<html>
-<style>
-label{
-width: 4em;
-float: left;
-text-align: right;
-margin-right: 0.5em;
-display: block
-}
-</style>
-<head>
-  <title>LizardRock Contact</title>
-  <link href="site.css" rel="stylesheet">
-</head>
-
-<body>
-
-  <nav id="nav01"></nav>
-
-  <div id="main">
-  <h1><img src="Logo.png" alt="LizardRock Symbol" 
-style="width:50px;height:50px">Contact got</h1>
-  <p>
-  
-  your contactness has been gotten<br>
-  here is contact info: <br>
-  
-  </p>
-
-<p>
+<?php
 
 
- 
-  </div>
-<footer id="foot01"></footer>
-  						
 
-<script src="script.js"></script>
+$name = Trim(stripslashes($_POST['name']));
+$email = Trim(stripslashes($_POST['email']));
+$message = Trim(stripslashes($_POST['message']));
 
-</body>
-</html>
+
+//eventually this code will write the info to a file on the server
+
+
+
+header("Location: http://lizardrock.github.io/contactDone.html"); 
+
+
+?>
