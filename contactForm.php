f4ec83bcea328ff9324e1cb31c09e98bd52e315a
LizardRock <lizardi.designs@gmail.com>
2015-06-19 20:34:04 -0400

    testing web form PHP

---
+++ contactForm.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+<style>
+label{
+width: 4em;
+float: left;
+text-align: right;
+margin-right: 0.5em;
+display: block
+}
+</style>
+<head>
+  <title>LizardRock Contact</title>
+  <link href="site.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <nav id="nav01"></nav>
+
+  <div id="main">
+  <h1><img src="Logo.png" alt="LizardRock Symbol" 
+style="width:50px;height:50px">Contact got</h1>
+  <p>
+  
+  your contactness has been gotten<br>
+  here is contact info: <br>
+  <?php 
+  $name = $_POST["name"]; 
+$email = $_POST["email"]; 
+$comment = $_POST["comment"]; 
+echo $name; 
+echo $email; 
+echo $comment; 
+?>
+  </p>
+
+<p>
+
+
+ 
+  </div>
+<footer id="foot01"></footer>
+  						
+
+<script src="script.js"></script>
+
+</body>
+</html>
